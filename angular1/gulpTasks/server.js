0d16012bab2ba42cbcd755b90ec98b6c0ab76c62
jersonjanke <jersonjanke@gmail.com>
2017-02-07 22:57:24 -0200

    Alteração nas portas

--- angular1/gulpTasks/server.js
+++ angular1/gulpTasks/server.js
@@ -5,7 +5,7 @@ const webserver = require('gulp-webserver')
 gulp.task('server', ['watch'], function () {
     gulp.src('public').pipe(webserver({
         livereload: true,
-        port: 3000,
+        port: 80,
         open: true
     }))
 })
